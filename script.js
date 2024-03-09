09a37b0d73e8f07e5a26cb4cccb8c7bef7b540ae
roosiu <roosiu@gmail.com>
2024-03-09 20:36:27 +0100

    upgrade

--- script.js
+++ script.js
@@ -1,14 +1,61 @@
+/**
+ * Generates an avatar based on the input text and size, and appends it to the specified element.
+ *
+ * @param {string} text - the input text used for generating the avatar
+ * @param {number} size - the size of the avatar
+ * @param {HTMLElement} el - the element to which the avatar will be appended
+ */
 function generateAvatar(text, size, el) {
   let svg = `<svg width="${size}" height="${size}"  viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">`;
-  for (let index = 0; index < 3; index++) {
-    const r = Math.floor(Math.random() * 100);
-    svg += `<circle cx="${Math.floor(Math.random() * 100)}" cy="${Math.floor(
-      Math.random() * 100
-    )}" r="${Math.floor(Math.random() * 50)}" fill="${
-      getMoreColor(textToColor(text))[index]
-    }"/>`;
-  }
+  const colorPattern = getMoreColor(textToColor(text));
+  const textLength = text.length;
 
+  const variaton = Math.floor(text.charCodeAt(0) % 30);
+  const variaton2 = Math.floor(text.charCodeAt(0) % 100);
+
+  ///triangles
+  svg += `<polygon points="${variaton2},0 ${
+    100 - variaton2
+  } ,0 50,${variaton}" fill="${colorPattern[2]}"/>`;
+  svg += `<polygon points="${variaton2},100 ${100 - variaton2},100 50,${
+    100 - variaton
+  }" fill="${colorPattern[2]}"/>`;
+  svg += `<polygon points="0,${
+    100 - variaton2
+  } 0,${variaton2} ${variaton},50" fill="${colorPattern[2]}"/>`;
+  svg += `<polygon points="100,${100 - variaton2} 100,${variaton2} ${
+    100 - variaton
+  },50" fill="${colorPattern[2]}"/>`;
+  ///triangles2
+  svg += `<polygon points="${variaton2 * 3},0 ${100 - variaton2 * 3} ,0 50,${
+    variaton / 2
+  }" fill="${colorPattern[1]}"/>`;
+  svg += `<polygon points="${variaton2 * 3},100 ${100 - variaton2 * 3},100 50,${
+    100 - variaton / 2
+  }" fill="${colorPattern[1]}"/>`;
+  svg += `<polygon points="0,${100 - variaton2 * 3} 0,${variaton2 * 3} ${
+    variaton / 2
+  },50" fill="${colorPattern[1]}"/>`;
+  svg += `<polygon points="100,${100 - variaton2 * 3} 100,${variaton2 * 3} ${
+    100 - variaton / 2
+  },50" fill="${colorPattern[1]}"/>`;
+
+  /// lines
+  svg += `<line x1="0" y1="0" x2="100" y2="100" stroke="${
+    colorPattern[0]
+  }" stroke-width="${(variaton * 2) / variaton2}" />`;
+  svg += `<line x1="100" y1="0" x2="0" y2="100" stroke="${
+    colorPattern[0]
+  }" stroke-width="${(variaton * 2) / variaton2}" />`;
+
+  /// circle
+  for (let i = 0; i < 3; i++) {
+    const charCode = text.charCodeAt(i % textLength);
+    const cx = 50;
+    const cy = 50;
+    const r = ((charCode + i) * 3) % 30;
+    svg += `<circle cx="${cx}" cy="${cy}" r="${r}" fill="${colorPattern[i]}"/>`;
+  }
   svg += "</svg>";
   el.innerHTML = svg;
 }
@@ -32,6 +79,12 @@ function textToColor(text) {
   return color;
 }
 
+/**
+ * Generate a color pattern based on the input color.
+ *
+ * @param {string} inputColor - The input color to generate the pattern from.
+ * @return {array} An array containing the input color and three additional colors in the pattern.
+ */
 function getMoreColor(inputColor) {
   const colorPattern = [inputColor];
   for (let i = 0; i < 3; i++) {
@@ -73,21 +126,28 @@ const avatarText = document.getElementById("avatarText");
 const avatar = document.getElementById("avatar");
 const buttonGenerate = document.getElementById("buttonGenerate");
 const avatarSize = document.getElementById("avatarSize");
+const getWarning = document.querySelector(".warning");
+
+avatarText.addEventListener("keyup", function (event) {
+  getWarning.style.display = "none";
+  if (event.key === "Enter") {
+    buttonGenerate.click();
+  }
+});
 
 buttonGenerate.addEventListener("click", function () {
-  getMoreColor(textToColor(avatarText.value)).forEach((element) => {
-    console.log(element);
-    const div = document.createElement("div");
-    div.style.width = "50px";
-    div.style.height = "50px";
-    div.style.backgroundColor = element;
-    div.style.display = "inline-block";
-    div.style.marginRight = "10px";
-    document.body.appendChild(div);
-  });
-  generateAvatar(avatarText.value, avatarSize.value, avatar);
+  if (avatarText.value) {
+    generateAvatar(avatarText.value, avatarSize.value, avatar);
+  } else {
+    getWarning.style.display = "block";
+  }
 });
+
 const text = document.querySelectorAll("[data-genav_text]");
+/**
+ * Executes the provided function when the window is fully loaded.
+ * @param {function} generateAvatar - The function to execute when the window is fully loaded.
+ */
 window.onload = function () {
   if (text.length) {
     text.forEach(function (el) {
